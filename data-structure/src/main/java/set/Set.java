876bd45f117120bc3947f09b38786d0344131bb4
Ximing Xing <ximingxing@gmail.com>
2020-11-26 21:29:39 +0800

    update project

---
+++ data-structure/src/main/java/set/Set.java
@@ -0,0 +1,18 @@
+package set;
+
+/**
+ * Description: Set interface
+ * Created By xxm
+ */
+public interface Set<E> {
+
+    void add(E e);
+
+    void remove(E e);
+
+    boolean contains(E e);
+
+    int getSize();
+
+    boolean isEmpty();
+}
---
+++ graph/src/test/java/basic/AdjSetTest.java
@@ -0,0 +1,19 @@
+package basic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Description:
+ * Created By xxm
+ */
+public class AdjSetTest {
+
+    @Test
+    public void AdjSet(){
+        AdjSet adjSet = new AdjSet("src/test/java/graph/basic/g.txt");
+        System.out.println(adjSet);
+    }
+
+}
