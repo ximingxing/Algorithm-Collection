e04a4148342f5144406d9c6dc61ae518fc4de1c0
ximing <ximingxing@gmail.com>
2020-12-05 19:41:31 +0800

    #Sort: add selection sort

--- algorithm-sort/src/main/java/sort/primary/SelectionSort.java
+++ algorithm-sort/src/main/java/sort/primary/SelectionSort.java
@@ -1,5 +1,28 @@
 package sort.primary;
 
-public class SelectionSort {
+import sort.Sort;
 
+public class SelectionSort extends Sort {
+
+    @Override
+    public void sortMethod(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++)
+                if (less(a[j], a[min])) min = j;
+            exch(a, i, min);
+        }
+    }
+
+    public static void sort(Comparable[] a) {
+        SelectionSort sort = new SelectionSort();
+        sort.sortMethod(a);
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{8, 6, 7, 1, 2, 3, 9, 0, 8, 4, 5};
+        SelectionSort.sort(a);
+        assert SelectionSort.isSorted(a);
+        SelectionSort.show(a);
+    }
 }
--- algorithm-sort/src/test/java/sort/primary/SelectionSortTest.java
+++ algorithm-sort/src/test/java/sort/primary/SelectionSortTest.java
@@ -1,4 +1,16 @@
+package sort.primary;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class SelectionSortTest {
-  
+
+    @Test
+    public void sort() {
+        Integer[] a = new Integer[]{8, 6, 7, 1, 2, 3, 9, 0, 8, 4, 5};
+        SelectionSort.sort(a);
+        assert SelectionSort.isSorted(a);
+        SelectionSort.show(a);
+    }
 }
